bed782a3157baf887e2042e396bfb5225afcfd1f
KubaG1974 <kuba.gorzynski@gmail.com>
2023-06-14 21:15:07 +0200

    Added ResourceStorage.cs

---
+++ ResourceStorage.cs
@@ -0,0 +1,60 @@
+using System;
+
+namespace ResourceApp
+{
+    public class ResourcesStorage
+    {
+        private static ResourcesStorage _instance;
+
+        private ResourcesStorage()
+        {
+            Energy = 0;
+            Coal = 0;
+            Uranium = 0;
+            Helium = 0;
+            Antimatter = 0;
+        }
+
+        public static ResourcesStorage Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new ResourcesStorage();
+
+                return _instance;
+            }
+        }
+
+        public int Energy { get; private set; }
+        public int Coal { get; private set; }
+        public int Uranium { get; private set; }
+        public int Helium { get; private set; }
+        public int Antimatter { get; private set; }
+
+        public void AddEnergy(int amount)
+        {
+            Energy += amount;
+        }
+
+        public void AddCoal(int amount)
+        {
+            Coal += amount;
+        }
+
+        public void AddUranium(int amount)
+        {
+            Uranium += amount;
+        }
+
+        public void AddHelium(int amount)
+        {
+            Helium += amount;
+        }
+
+        public void AddAntimatter(int amount)
+        {
+            Antimatter += amount;
+        }
+    }
+}
